b8d1c67a75f021f323768667bcefb9efd56bbce6
bengris32 <bengris32@protonmail.ch>
2023-07-21 13:10:34 +0100

    selene: Ship MediaTek cgroup/task_profiles configuration
    * MediaTek ship their own modified task_profiles and
      use the V30 cgroups configuration.
    
    Change-Id: I15245ea87b00925597db65f1564e808a094c937a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: hasan6034 <hasanbasrioy@gmail.com>

--- device.mk
+++ device.mk
@@ -111,6 +111,11 @@ PRODUCT_PACKAGES += \
     libpiex_shim \
     libshim_utils
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend
