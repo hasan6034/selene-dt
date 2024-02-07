b5da1241f4f23eed752306cf2fa55ce85ed77f11
Bruno Martins <bgcngm@gmail.com>
2024-02-07 15:17:11 +0100

    selene: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c
    Signed-off-by: hasan6034 <hasanbasrioy@gmail.com>

--- device.mk
+++ device.mk
@@ -329,7 +329,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.mediatek-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
