bd59913cd1aa4b7a38d263fc5c7568167203362a
Matsvei Niaverau <matvej2002@gmail.com>
2024-02-09 09:19:07 +0100

    selene: Update `kpoc_charger` blobs from TECNO-CK9n
    Change-Id: Ia510f94a0b7b324253a39e8f962244165fe932eb
    Signed-off-by: hasan6034 <hasanbasrioy@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -92,9 +92,6 @@ function blob_fixup {
         vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
             "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
             ;;
-        lib/libshowlogo.so)
-            "${PATCHELF}" --add-needed "libshim_showlogo.so" "${2}"
-            ;;
         vendor/lib/libMtkOmxVdecEx.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v32.so" "$2"
             ;;
